94f8fa065a0f164e88cee0f23f245f0124b1aff7
Harsh2813 <prateektiwari.pk55@gmail.com>
2023-12-07 13:06:32 +0530

    added multiple states and event handlers and submit button

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -1,11 +1,55 @@
+import React, {useState} from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () =>{
+
+    const [enteredTitle, setEnteredTitle] = useState('')
+    const [enteredAmount, setEnteredAmount] = useState('')
+    const [enteredDate, setEnteredDate] = useState('')
+ //Instead of using all separate state we can use a single combined state because all this state is for common form
+    /*
+    const [userInput, setUserInput] = useState({
+        enteredTitle: '',
+        enteredAmount: '',
+        enteredDate: '',
+    });
+    */
     const titleChangeHandler = (event) =>{
-        console.log(event.target.value);
+        setEnteredTitle(event.target.value);
+
+        // setUserInput({// this is not ideal approach because here we depend on single state for all input objects
+        //     ...userInput, // this can be give outdated state data as state is async as sheduled in callback queue
+        //     enteredTitle: event.target.value // but below approch gives updated data and ideal for single state
+        // })
+
+        // setUserInput((prevState) =>{ // this prevState automatically takes above state's object that's why w                               we did ...preState not ...userInput becuase all we get in preState
+        //     return {...prevState, enteredTitle: event.target.value};
+        // });
+
+        // jo bhi onChange event se value milegi wo enteredTitle me jayegi enteredTitle uper state me initilized h aur baki amount date khali rahega becuase ye titleChangeHandler me amountChange me spread operator se sb copy krke sirf amount store karenge aur return
+        //we can we do this method if we take single state and NOTE:-- React shedules state's update so by using this method react will gaurantee that the state snapshot we used as innerfunction setUserInput will always be latest and updated snapshot state keep all scheduled state updated in mind
     };
+    
+    const amountChangeHandler = (event) =>{
+        setEnteredAmount(event.target.value);
+    };
+    
+    const dateChangeHandler = (event) =>{
+        setEnteredDate(event.target.value);
+    };
+
+    const SubmitHandler = (event) =>{
+        event.preventDefault();
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate),
+        };
+        console.log(expenseData);
+    }
+
     return (
-        <form>
+        <form onSubmit={SubmitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
@@ -13,11 +57,11 @@ const ExpenseForm = () =>{
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='number' min="0.01" step = '0.01'/>
+                    <input type='number' min="0.01" step = '0.01' onChange={amountChangeHandler}/>
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' min= '2019-01-01' max= '2022-12-31'/>
+                    <input type='date' min= '2019-01-01' max= '2022-12-31' onChange={dateChangeHandler}/>
                 </div>
             </div>
             <div className='new-expense__actions'>
